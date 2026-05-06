f52ff860986af161c0c1c2bfd8e1e80c5c14877a
venkatarangan thirumalai <venkatarangan@users.noreply.github.com>
2026-05-06 15:22:22 +0530

    the latest windows build with the changes made today on menu structure changes, new features, following the mac os build

--- neight.py
+++ neight.py
@@ -21,7 +21,7 @@ from typing import Optional
 from urllib.parse import quote_plus
 
 # Version information
-VERSION = "2026.022"
+VERSION = "2026.023"
 
 DEFAULT_GOOGLE_SEARCH_URL_PREFIX = "https://www.google.com/search?q="
 DEFAULT_SORKUVAI_SEARCH_URL_PREFIX = "https://sorkuvai.tn.gov.in/?q="
