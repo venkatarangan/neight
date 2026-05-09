c4c00f3a8a4fcab9d818b6246996e2f42c5fa972
venkatarangan thirumalai <venkatarangan@users.noreply.github.com>
2026-05-09 21:55:30 +0530

    windows build

--- neight.py
+++ neight.py
@@ -23,7 +23,7 @@ from typing import Optional
 from urllib.parse import quote_plus
 
 # Version information
-VERSION = "2026.034"
+VERSION = "2026.035"
 
 DEFAULT_GOOGLE_SEARCH_URL_PREFIX = "https://www.google.com/search?q="
 DEFAULT_SORKUVAI_SEARCH_URL_PREFIX = "https://sorkuvai.tn.gov.in/?q="
