6dd0257514b78e0a43c7aea85c8726abe29fe684
Venkatarangan Thirumalai <venkatarangan@hotmail.com>
2026-05-11 22:38:58 +0530

    new mac build with all the changes

--- neight.py
+++ neight.py
@@ -24,7 +24,7 @@ from typing import Optional
 from urllib.parse import quote_plus
 
 # Version information
-VERSION = "2026.047"
+VERSION = "2026.048"
 
 DEFAULT_GOOGLE_SEARCH_URL_PREFIX = "https://www.google.com/search?q="
 DEFAULT_SORKUVAI_SEARCH_URL_PREFIX = "https://sorkuvai.tn.gov.in/?q="
